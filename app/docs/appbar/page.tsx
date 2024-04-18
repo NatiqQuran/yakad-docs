60ae42f2d1ed1d9f62a181ad66ec309b3995d103
solsonightkeeper <solsonightkeeper@gmail.com>
2024-04-18 16:41:19 +0330

    AppBar

--- app/docs/appbar/page.tsx
+++ app/docs/appbar/page.tsx
@@ -3,20 +3,51 @@ import {
   Button,
   Container,
   Footer,
+  Card,
   Main,
   Page,
   Row,
   Spacer,
   Stack,
+  Hr,
+  SvgIcon,
 } from "@yakad/ui";
+import Symbol from "@yakad/symbols";
 import Link from "next/link";
+import LogoIcon from "@/app/logoicon";
 
 export default function DocsAppbar() {
   return (
     <Container maxWidth="lg">
+      <h1 style={{fontSize:"3rem"}}>AppBar</h1>
+      <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h1>
+      <Hr />
+      <h1>Examples 1:</h1>
       <AppBar>
-        <h1>Yakad Appbar</h1>
+        <h1>Appbar</h1>
       </AppBar>
+
+      <h1>Examples 2:</h1>
+      <AppBar>
+      <Button icon={<Symbol icon="menu" />} />
+        <h1>AppBar</h1>
+      </AppBar>
+
+      <h1>Examples 3:</h1>
+      <AppBar>
+        <SvgIcon size={4}>
+          <LogoIcon />  
+        </SvgIcon>
+        <h1>AppBar</h1>
+        <Button>Github</Button>
+        <Button>About</Button>
+        <Spacer />
+        <Button variant="outlined" icon={<Symbol icon="login" />} >Login</Button>
+        
+
+      </AppBar>
+
+
     </Container>
   );
 }
