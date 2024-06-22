f2bc37d543939a2ff8953bbddd8b5c136712455f
Martis5 <sjadk757@gmail.com>
2024-06-22 16:34:14 +0330

    fix som thing

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -13,15 +13,20 @@ import {
 } from "@yakad/ui";
 import ExampleProjects from "./example";
 import Installation from "./installation";
+import Link from "next/link";
 
 export default function Docs() {
   return (
     <div>
       <Container maxWidth="md">
-        <h1 style={{ fontSize: "3rem" }}>Owerview #</h1>
+        <h1 style={{ fontSize: "3rem" }}>Owerview</h1>
         <p style={{ fontSize: "20px" }}>
-          We bulit <b>Yakad</b> to meet the UI/UX needs of the talk project. By
-          with the MIT licens, we also provided the open source community.
+          We bulit <b>Yakad</b> to meet the UI/UX needs of the{" "}
+          <Link target="_blank" href="https://natiq.net">
+            Natiq
+          </Link>{" "}
+          project. By with the MIT licens, we also provided the open source
+          community.
         </p>
         <Hr />
       </Container>
