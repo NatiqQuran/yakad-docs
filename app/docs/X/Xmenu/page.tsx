df788c661289d91b9ff1649b88e0a75411dabb0a
leycci <solsonightkeeper@gmail.com>
2024-06-26 17:01:17 +0330

    create Xbackground page

---
+++ app/docs/X/Xmenu/page.tsx
@@ -0,0 +1,9 @@
+import { AppBar, Button, Container } from "@yakad/ui";
+2;
+export default function Page() {
+  return (
+    <Container maxWidth="md">
+      <h1>Xmenu</h1>
+    </Container>
+  );
+}
---
+++ app/docs/X/Xtable/page.tsx
@@ -0,0 +1,9 @@
+import { AppBar, Button, Container } from "@yakad/ui";
+2;
+export default function Page() {
+  return (
+    <Container maxWidth="md">
+      <h1>Xtable</h1>
+    </Container>
+  );
+}
