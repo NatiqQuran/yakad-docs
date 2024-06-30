b9887147d85ec1cbc8b1e8434eb8abfeffd2850d
Martis5 <sjadk757@gmail.com>
2024-06-30 10:02:39 +0330

    fix grid

--- app/docs/ui/grid/page.tsx
+++ app/docs/ui/grid/page.tsx
@@ -1,9 +1,12 @@
 import {
+  Button,
   Card,
   Container,
   GridContainer,
   GridItem,
   Hr,
+  Row,
+  Spacer,
   Table,
   Tbody,
   Td,
@@ -15,15 +18,26 @@ import {
 export default function Page() {
   return (
     <Container maxWidth="md">
-      <h1 style={{ fontSize: "2.5rem" }}>Grid</h1>
-      <p style={{ fontSize: "1.5rem" }}>
-        With grid, you can divide the page from 1 to 12 parts, easily edit your
-        site page.
-      </p>
+      <h1>Grid</h1>
+      <Hr />
+      <p>Discription</p>
+      <Card>
+        <Row>
+          <span>
+            <code>{'import { GridContainer, GridItem } from "@yakad/ui"'}</code>
+            <br />
+            <br />
+            <code>
+              {"<GridContainer><GridItem></GridItem></GridContainer>"}
+            </code>
+          </span>
+          <Spacer />
+          <Button icon="content_copy"></Button>
+        </Row>
+      </Card>
 
-      <p style={{ fontSize: "1.5rem" }}>
-        Properties that are accepted as attributes:(Grid Item)
-      </p>
+      <h2>Properties</h2>
+      <p>Properties that are accepted as attributes:(Grid Item)</p>
       <Table>
         <Thead>
           <Tr>
@@ -54,9 +68,7 @@ export default function Page() {
           </Tr>
         </Tbody>
       </Table>
-      <p style={{ fontSize: "1.5rem" }}>
-        Properties that are accepted as attributes:(Grid Container)
-      </p>
+      <p>Properties that are accepted as attributes:(Grid Container)</p>
       <Table>
         <Thead>
           <Tr>
@@ -66,17 +78,118 @@ export default function Page() {
         </Thead>
         <Tbody>
           <Tr>
-            <Td>Columns</Td>
+            <Td>columns</Td>
             <Td>10 | 12</Td>
           </Tr>
           <Tr>
-            <Td>Gap (rem)</Td>
+            <Td>gap (rem)</Td>
             <Td>number</Td>
           </Tr>
         </Tbody>
       </Table>
 
-      <h1 style={{ fontSize: "1.5rem", padding: "2rem" }}>Example 1:</h1>
+      <h3>columns</h3>
+      <p>
+        With this property, you decide that the page you want is divided into
+        several columns.
+      </p>
+      <GridContainer columns={10}>
+        <GridItem xs={2} sm={2} xl={1}>
+          <Card></Card>
+        </GridItem>
+        <GridItem xs={2} sm={2} xl={1}>
+          <Card></Card>
+        </GridItem>
+        <GridItem xs={2} sm={2} xl={1}>
+          <Card></Card>
+        </GridItem>
+        <GridItem xs={2} sm={2} xl={1}>
+          <Card></Card>
+        </GridItem>
+        <GridItem xs={2} sm={2} xl={1}>
+          <Card></Card>
+        </GridItem>
+        <GridItem xs={2} sm={2} xl={1}>
+          <Card></Card>
+        </GridItem>
+        <GridItem xs={2} sm={2} xl={1}>
+          <Card></Card>
+        </GridItem>
+        <GridItem xs={2} sm={2} xl={1}>
+          <Card></Card>
+        </GridItem>
+        <GridItem xs={2} sm={2} xl={1}>
+          <Card></Card>
+        </GridItem>
+        <GridItem xs={2} sm={2} xl={1}>
+          <Card></Card>
+        </GridItem>
+      </GridContainer>
+      <Hr />
+      <GridContainer columns={12}>
+        <GridItem xs={2} sm={1} xl={1}>
+          <Card></Card>
+        </GridItem>
+        <GridItem xs={2} sm={1} xl={1}>
+          <Card></Card>
+        </GridItem>
+        <GridItem xs={2} sm={2} xl={1}>
+          <Card></Card>
+        </GridItem>
+        <GridItem xs={2} sm={2} xl={1}>
+          <Card></Card>
+        </GridItem>
+        <GridItem xs={2} sm={2} xl={1}>
+          <Card></Card>
+        </GridItem>
+        <GridItem xs={2} sm={2} xl={1}>
+          <Card></Card>
+        </GridItem>
+        <GridItem xs={2} sm={2} xl={1}>
+          <Card></Card>
+        </GridItem>
+        <GridItem xs={2} sm={2} xl={1}>
+          <Card></Card>
+        </GridItem>
+        <GridItem xs={2} sm={2} xl={1}>
+          <Card></Card>
+        </GridItem>
+        <GridItem xs={2} sm={2} xl={1}>
+          <Card></Card>
+        </GridItem>
+        <GridItem xs={2} sm={2} xl={1}>
+          <Card></Card>
+        </GridItem>
+        <GridItem xs={2} sm={2} xl={1}>
+          <Card></Card>
+        </GridItem>
+      </GridContainer>
+
+      <h3>gap</h3>
+      <p>The use of the gap is to create a gap between the grid items.</p>
+      <GridContainer gap={10}>
+        <GridItem>
+          <Card></Card>
+        </GridItem>
+        <GridItem>
+          <Card></Card>
+        </GridItem>
+      </GridContainer>
+
+      <h3>xl</h3>
+      <p></p>
+
+      <h3>sm</h3>
+      <p></p>
+
+      <h3>md</h3>
+      <p></p>
+
+      <p>
+        With grid, you can divide the page from 1 to 12 parts, easily edit your
+        site page.
+      </p>
+      <h1>Example 1:</h1>
 
       <GridContainer>
         <GridItem md={10} lg={6} xl={6}>
