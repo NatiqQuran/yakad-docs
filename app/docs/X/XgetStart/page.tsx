340d05a23ee2b9adcdf32601e37aa59471313840
leycci <solsonightkeeper@gmail.com>
2024-07-02 10:01:35 +0330

    Add favicon,create Xpage

--- app/docs/X/XgetStart/page.tsx
+++ app/docs/X/XgetStart/page.tsx
@@ -1,8 +1,65 @@
-import { AppBar, Button, Container } from "@yakad/ui";
+import LogoIcon from "@/app/logoicon";
+import {
+  Button,
+  Card,
+  Container,
+  Hr,
+  Tr,
+  Table,
+  Thead,
+  Th,
+  Tbody,
+  Td,
+  GridContainer,
+  GridItem,
+  AppBar,
+} from "@yakad/ui";
+import { XgetStart } from "@yakad/x";
+import { Children } from "react";
+
 export default function Page() {
   return (
     <Container maxWidth="md">
       <h1>XgetStart</h1>
+      <Hr />
+      <p>A simple fast intro box</p>
+      <Card>
+        <code>{'import { XgetStart } from "@yakad/x";'}</code>
+        <br />
+        <br />
+        <code>{"<XgetStart>Contenet</XgetStart>"}</code>
+      </Card>
+      <h2>Properties</h2>
+      <p>Properties that are accepted as attributes:</p>
+      <Table>
+        <Thead>
+          <Tr>
+            <Th>Property</Th>
+            <Th>Value</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          <Tr>
+            <Td>logo</Td>
+            <Td>_</Td>
+          </Tr>
+          <Tr>
+            <Td>children</Td>
+            <Td>_</Td>
+          </Tr>
+        </Tbody>
+      </Table>
+      <h2>Properties</h2>
+      <h3>variant</h3>
+      <GridContainer>
+        <GridItem sm={12} xl={6}>
+          <XgetStart logo={<LogoIcon />}>
+            <h1>XgetStart</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
+            <Button variant="filledtonal">XgetStart</Button>
+          </XgetStart>
+        </GridItem>
+      </GridContainer>
     </Container>
   );
 }
--- app/docs/navList.tsx
+++ app/docs/navList.tsx
@@ -44,6 +44,7 @@ export default function NavList() {
         },
         { name: "XgetStart", onclick: () => router.push("/docs/X/XgetStart") },
         { name: "XloginBox", onclick: () => router.push("/docs/X/XloginBox") },
+
         { name: "Xmenu", onclick: () => router.push("/docs/X/Xmenu") },
         { name: "Xpanel", onclick: () => router.push("/docs/X/Xpanel") },
         { name: "Xtable", onclick: () => router.push("/docs/X/Xtable") },
